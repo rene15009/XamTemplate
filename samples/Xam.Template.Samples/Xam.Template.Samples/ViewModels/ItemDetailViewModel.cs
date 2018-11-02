63c92405c02636df79787f02a996c77022d12485
René Pacios <rene15009@hotmail.com>
2018-11-02 19:54:33 +0100

    Added Sample Project

---
+++ samples/Xam.Template.Samples/Xam.Template.Samples/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xam.Template.Samples.Models;
+
+namespace Xam.Template.Samples.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

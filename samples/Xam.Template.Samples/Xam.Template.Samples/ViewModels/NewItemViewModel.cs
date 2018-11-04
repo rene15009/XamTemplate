55b10b8e2cd80e5a20c794789857c4fdecc90160
René Pacios <rene15009@hotmail.com>
2018-11-04 21:16:43 +0100

    .check antes de enlazarNewItem a datos

--- samples/Xam.Template.Samples/Xam.Template.Samples/ViewModels/NewItemViewModel.cs
+++ samples/Xam.Template.Samples/Xam.Template.Samples/ViewModels/NewItemViewModel.cs
@@ -4,20 +4,32 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Rene.Xam.Extensions.Base;
 using Rene.Xam.Extensions.Bootstrapping.Interfaces;
 using Xam.Template.Samples.Models;
+using Xamarin.Forms;
 
 namespace Xam.Template.Samples.ViewModels
 {
     public class NewItemViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+
+
         public Item Item { get; set; } = new Item();
+
+        public ICommand BtnSaveClick { get; set; }
 
         public NewItemViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            BtnSaveClick=new Command(OnSave);
+        }
+
+        private void OnSave()
+        {
+            
         }
     }
 }

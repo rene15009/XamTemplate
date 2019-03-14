4385bba405deabbaf632a465d7698b3c1355b416
René Pacios <rene15009@hotmail.com>
2019-03-14 21:26:07 +0100

    Added MessaginServices Wrappper

---
+++ src/Rene.Xam.Extensions/Bootstrapping/Interfaces/IMessagingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rene.Xam.Extensions.Bootstrapping.Interfaces
+{
+    public interface IMessagingService
+    {
+        void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class;
+        void Send<TSender>(TSender sender, string message) where TSender : class;
+        void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null) where TSender : class;
+        void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = null) where TSender : class;
+        void Unsubscribe<TSender, TArgs>(object subscriber, string message) where TSender : class;
+        void Unsubscribe<TSender>(object subscriber, string message) where TSender : class;
+    }
+}
---
+++ src/Rene.Xam.Extensions/Bootstrapping/Services/MessagingService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Rene.Xam.Extensions.Bootstrapping.Interfaces;
+using Xamarin.Forms;
+
+namespace Rene.Xam.Extensions.Bootstrapping.Services
+{
+    public class MessagingService : IMessagingService
+    {
+       
+
+        public void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class
+        {
+            MessagingCenter.Send(sender,message,args);
+        }
+
+        public void Send<TSender>(TSender sender, string message) where TSender : class
+        {
+           MessagingCenter.Send(sender, message);
+        }
+
+        public void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = default(TSender)) where TSender : class
+        {
+           MessagingCenter.Subscribe(subscriber, message, callback, source);
+        }
+
+        public void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = default(TSender)) where TSender : class
+        {
+           MessagingCenter.Subscribe(subscriber, message, callback, source);
+        }
+
+        public void Unsubscribe<TSender, TArgs>(object subscriber, string message) where TSender : class
+        {
+            MessagingCenter.Unsubscribe<TSender,TArgs>(subscriber, message);
+        }
+
+        public void Unsubscribe<TSender>(object subscriber, string message) where TSender : class
+        {
+            MessagingCenter.Unsubscribe<TSender>(subscriber, message);
+        }
+    }
+}
